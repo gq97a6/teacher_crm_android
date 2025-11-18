baba24e135a048901599b5b401f86515d4943762
gq97a6 <sz.kg@protonmail.com>
2025-11-18 10:19:04 +0100

    Settings and Groups app bar added.

---
+++ src/main/kotlin/org/labcluster/crm/composable/group/GroupAppBar.kt
@@ -0,0 +1,16 @@
+package org.labcluster.crm.composable.group
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.labcluster.crm.PreviewSample
+import org.labcluster.crm.shared.Mock
+import org.labcluster.crm.shared.model.Group
+
+@Preview
+@Composable
+private fun Preview() = PreviewSample { GroupScreen() }
+
+@Composable
+fun GroupAppBar(group: Group = Mock.groups.random()) {
+
+}
