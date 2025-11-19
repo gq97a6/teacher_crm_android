dc5277f1ab6d58c4687ea45e118369489cf997eb
gq97a6 <sz.kg@protonmail.com>
2025-11-19 20:49:23 +0100

    Topic app bar action removed. Topic view refactored.

--- src/main/kotlin/org/labcluster/crm/screen/topic/TopicView.kt
+++ src/main/kotlin/org/labcluster/crm/screen/topic/TopicView.kt
@@ -1,17 +1,25 @@
 package org.labcluster.crm.screen.topic
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import org.labcluster.crm.Mock.lorem
 import org.labcluster.crm.composable.PreviewScaffold
+import org.labcluster.crm.cs
 import org.labcluster.crm.screen.topic.compose.TopicAppBar
-import org.labcluster.crm.screen.topic.compose.TopicContent
+import kotlin.random.Random
 
 @Preview
 @Composable
@@ -24,11 +32,24 @@ fun TopicView(vm: TopicViewModel = viewModel()) {
 
     Scaffold(
         modifier = Modifier,
-        topBar = {
-            TopicAppBar(title = topic.name)
-        },
+        topBar = { TopicAppBar(title = topic.name) },
         contentWindowInsets = WindowInsets(left = 15.dp, right = 15.dp)
     ) { paddingValues ->
-        TopicContent(paddingValues)
+        LazyColumn(Modifier.padding(paddingValues)) {
+            items(20) {
+                Text(
+                    text = lorem.take(11),
+                    color = cs.onBackground,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Text(
+                    text = lorem.take(Random.nextInt(50, 400)),
+                    color = cs.onBackground
+                )
+
+                Spacer(Modifier.height(10.dp))
+            }
+        }
     }
 }
